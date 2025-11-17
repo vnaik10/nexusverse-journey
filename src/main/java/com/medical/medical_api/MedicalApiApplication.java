3f15c265fc6a965b7beddd3922e6d6da303772c7
Vikas Naik <naikvikas111@gmail.com>
2025-11-18 05:17:50 +0530

    chore: include .gitignore and application base class for Phase 2 Step 4

---
+++ src/main/java/com/medical/medical_api/MedicalApiApplication.java
@@ -0,0 +1,14 @@
+package com.medical.medical_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MedicalApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalApiApplication.class, args);
+	}
+}
+
+
